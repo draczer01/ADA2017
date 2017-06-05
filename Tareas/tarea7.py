ebede67c8ef27acdb7af13faaeea81b853c2c63d
jhernandez <jhernandez@ppgodel.mx>
2017-06-04 21:53:02 -0500

    fin tarea7

--- Tareas/tarea7.py
+++ Tareas/tarea7.py
@@ -5,30 +5,33 @@ from files import InstanciesGenerator
 from heapq import heappop, heappush
 import random
 
-no_vertices = 15 
+no_cycles = 10
+no_vertices = 15
 
 
-dd = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.uniform, 1, 2 )
-dw = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.uniform, 1, 2)
-generador = InstanciesGenerator.GraphInstancesGenerator(graphtype = InstanciesGenerator.GraphTypes.tree,distribution_weight = dw,distribution_degree = dd, directed = True )
+dd = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.uniform, 1, 25 )
+dw = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.uniform, 1, 5)
+generador = InstanciesGenerator.GraphInstancesGenerator(graphtype = InstanciesGenerator.GraphTypes.connected,distribution_weight = dw,distribution_degree = dd, directed = True )
 
-g = generador.generateInstance('Test', no_vertices, 20)
+g = generador.generateInstance('Test', no_vertices, no_vertices * 1.5)
 print(g)
 #print('con',g.connected)
-if g.connected:
+for i in range(no_cycles):
+
     scover = set()
     covered = set()
-    while len(covered) < g.cardinal:
-        print(len(covered), g.cardinal)
-        cl = []
-        for v in g.vertices:
-            a = (len(set(g[v].neighbors) - covered),v)
-            cl.append(a)
-        m, vv = max(cl, key=lambda k:k[0])
-        scover.add(vv)
-        covered.add(vv)
-        for x in g[vv].neighbors.keys():
-            covered.add(x)
-    print('res',scover,len(scover))
-else:
-    print('not connected')        
+    
+    edg = list(g.getedges().keys())
+
+    while len(edg) > 0:
+#        print(len(edg))
+        se = random.choice(edg)
+        #print(se)
+        scover.add(se[0])
+        scover.add(se[1])
+        for e in edg:
+            if se[0] in e or se[1] in e:
+                #print(se, e, se[0] in e, se[1] in e, len(edg))
+                edg.remove(e)
+    
+    print('res ', i, ' ', scover, len(scover))
