ed68233ee9c98435bce1a35d395def7e37a904ac
ppGodel <tashiro86@gmail.com>
2017-06-08 22:38:13 -0500

    Comments
    Se agregaron comentarios

--- Tareas/tarea4BFS.py
+++ Tareas/tarea4BFS.py
@@ -1,18 +1,21 @@
 # BFS y DFS
 #archivos necesarios dentro de la carpeta files
-# graph.py, bucketfill.py, InstanciesGenerator.py
+# graph.py, InstanciesGenerator.py
+# aplicacion de BFS con un closeness centrality
 from files import graph
 from files import InstanciesGenerator
 import random
-# aplicacion de BFS con un closeness centrality
+# se crea una instacioa aleatoria, ene ste caso de un arbol
 dd = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.uniform, 10, 2)
 dw = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.uniform, 1, 5)
 generador = InstanciesGenerator.GraphInstancesGenerator(graphtype = InstanciesGenerator.GraphTypes.tree,distribution_weight = dw,distribution_degree = dd, directed = True )
 g = generador.generateInstance('Test', 10, 50)
-
+#se imprime el grafo creado
 print(g)
+
 lv = g.vertices
 av = random.choice(lv)
 
 cc = g.closenesscentrality(av)
+# se calcula la medida de centralidad por cercania de todos los nodos de ese grafo
 print([(x, g.closenesscentrality(x)) for x in g.vertices])
