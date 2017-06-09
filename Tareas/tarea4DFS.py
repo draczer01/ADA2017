91dd16f53ddfc2ec867ef34260288449b72b3bdf
ppGodel <tashiro86@gmail.com>
2017-06-08 22:43:03 -0500

    comentarios
    se agregaron los comentarios de las tarea

--- Tareas/tarea4DFS.py
+++ Tareas/tarea4DFS.py
@@ -1,7 +1,8 @@
 from files import bucketfill
 #archivos necesarios dentro de la carpeta files
 # bucketfill.py
+#para la creacion de esta tarea se necesita pygame instalado en la computadora
 
-#aplicacion de DFS con un bucket fill
+#aplicacion de DFS con un bucket fill, lee una imagen predefinida y a partir del lugar dnde se hace click, hace un dfs en la imagen tomando los los pixeles como nodos y siendo los pixeles vecinos up,down, left, right, con un click cambia el color a rojo o si el color ya es rojo, lo cambia a azul
 bucketfill.fill()
 
