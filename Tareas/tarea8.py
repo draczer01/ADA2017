abad14e0cc30e5246259a964c1859e84b7ede02b
ppGodel <tashiro86@gmail.com>
2017-06-12 08:56:16 -0500

    comentarios

--- Tareas/tarea8.py
+++ Tareas/tarea8.py
@@ -1,3 +1,5 @@
+# Transicion de fase
+# Se revisó la transicion de fase de la centralidad por cercania contra la densidad de una grafo no dirigido , ya que mostraba una transicion de fase al trabajar con grafos completos de 50 aristas o menos
 
 # generador de instancias de grafos
 from files import graph
@@ -5,7 +7,7 @@ from files import InstanciesGenerator
 import random
 import time
 
-
+#funcion que ejecuta y mide la centralidad por cercania para todos los nodos
 def exe(g,de=0):
     g#0 = generadortree.generateInstance(name + ' tree', n, m1)
     g.connected
@@ -16,17 +18,12 @@ def exe(g,de=0):
     cct =g.closenesscentrality()
     tt1 = time.clock()
     tc = (tt1-tt0)*1000.0
-    #print(g0.to_string(sv=False))
-    d = (g.density)*100.0
-    tt0 = time.clock()
-    bfst =g.breadthfirstsearch()
-    tt1 = time.clock()
-    tb = (tt1-tt0)*1000.0
-    print( str(de) , str(tc), str(tb) )
+    #informacion que se toma en cuenta, densidad y tiempo de ejecucion de la cc
+    print( str(de) , str(tc))
     del g
     return d
 
-
+#Definicion de los parametros,
 #Nombre
 name = 'test'
 direct = False
@@ -47,16 +44,20 @@ generadorconnected = InstanciesGenerator.GraphInstancesGenerator(graphtype = Ins
 
 generadortree = InstanciesGenerator.GraphInstancesGenerator(graphtype = InstanciesGenerator.GraphTypes.tree,distribution_weight = dw,distribution_degree = dd, directed = direct )
 
+#incio del ciclo
 for i in range(cycle):
+    # se genera un arbol, apra comenzar a medir el tiempo
     g0 = generadortree.generateInstance(name + ' tree', n, n-1)
     exe(g0, (g0.density)*100.0)
-    d = 5
+    #ahora, aumentando al densidad del grafo de 5 en 5, calcularemos su CC
+    d = 5    
     while d < 100:
         g1 = generadorconnected.generateInstance(name + ' connected', n, (n*(n-1)*d/100))
                 
         exe(g1,d)
         d += 5
-    
+    #por ultimo calcularmos el tiempo de un grafo completo
     gc = generadorcomplete.generateInstance(name + ' complete', n, n)
     exe(gc,(gc.density)*100.0)
-  
+
+# se generaron varios resultados para los valores de 50 aristas, mientras que para tamaños mayores se perdia la transicion de fase.
