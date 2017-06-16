771950f95446859113e6fbd8ba14a3d5dc797a20
Jose Hernandez <tashiro86@gmail.com>
2017-06-16 08:12:55 -0500

    finalizada tarea5

--- Tareas/resultadosTarea5/analisisTarea5.R
+++ Tareas/resultadosTarea5/analisisTarea5.R
@@ -1,10 +1,22 @@
-<<<<<<< HEAD
-datos = read.csv("result3Tarea5.dat", sep=" ", header=FALSE)
+datos = read.csv("result2Tarea5.dat", sep=" ", header=FALSE)
 a = aggregate(datos, by=list(datos$V2), mean)
 plot(a$Group.1/(a$V1*a$V1-1), a$V5, col="blue", pch=16, type="l",main="Comparación densidad grafo vs tiempo de ejecucion de FF shortest augmenting path a 205 nodos",xlab="Densida grafo", ylab="Tiempo de ejecucion")
-=======
+boxplot(datos$V5 ~ datos$V2)
+plot(a$Group.1/(a$V1*a$V1-1), a$V4, col="blue", pch=16, type="l",main="Comparación densidad grafo vs flujo por FF shortest augmenting path a 205 nodos",xlab="Densida grafo", ylab="Flujo")
+boxplot(datos$V4 ~ datos$V2)
+
+datos = read.csv("result3Tarea5.dat", sep=" ", header=FALSE)
+a = aggregate(datos, by=list(datos$V2), mean)
+plot(a$Group.1/(a$V1*a$V1-1), a$V5, col="blue", pch=16, type="l",main="Comparación densidad grafo vs tiempo de ejecucion de FF shortest augmenting path a 105 nodos",xlab="Densida grafo", ylab="Tiempo de ejecucion")
+boxplot(datos$V5 ~ datos$V2)
+plot(a$Group.1/(a$V1*a$V1-1), a$V4, col="blue", pch=16, type="l",main="Comparación densidad grafo vs flujo por FF shortest augmenting path a 105 nodos",xlab="Densida grafo", ylab="Flujo")
+boxplot(datos$V4 ~ datos$V2)
+
 datos = read.csv("resultTarea5.dat", sep=" ", header=FALSE)
 a = aggregate(datos, by=list(datos$V2), mean)
-plot(a$Group.1*100/a$V1, a$V5, col="blue", pch=16, type="l",main="Comparación tiempo entre la densidad del grafo",xlab="Densidad", ylab="Tiempo")
+plot(a$Group.1/(a$V1*a$V1-1), a$V5, col="blue", pch=16, type="l",main="Comparación densidad grafo vs tiempo de ejecucion de FF shortest augmenting path a 1005 nodos",xlab="Densida grafo", ylab="Tiempo de ejecucion")
 boxplot(datos$V5 ~ datos$V2)
->>>>>>> 56b72785ca48a714cdd687fd63f244fe1e3b66bf
+plot(a$Group.1/(a$V1*a$V1-1), a$V4, col="blue", pch=16, type="l",main="Comparación densidad grafo vs flujo por FF shortest augmenting path a 1005 nodos",xlab="Densida grafo", ylab="Flujo")
+boxplot(datos$V4 ~ datos$V2)
+
+
--- Tareas/tarea5.py
+++ Tareas/tarea5.py
@@ -5,14 +5,14 @@ from files import graph
 from files import InstanciesGenerator
 import random
 import time
-# se genera un grafo con densidad alta y con una cantidad de vertices alta
+# se genera un grafo con densidad alta y con una cantidad de vertices alta, para ver el desempeño del algoritmo con grafos diversos se probara con 105, 205 y 1005 nodos
 no_vertices = 205
 
 
 ddn = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.uniform, 1, no_vertices-1 )
 dw = InstanciesGenerator.Distribution(InstanciesGenerator.DistributionsTypes.normal, 15, 3)
 generadorcon = InstanciesGenerator.GraphInstancesGenerator(graphtype = InstanciesGenerator.GraphTypes.connected,distribution_weight = dw,distribution_degree = ddn, directed = False )
-
+#se probaran diferentes densidades del grafo para ver el desempeño cuando se va a acercando a la densidad de un grafo completo
 density = [0.8,0.85,0.90,0.95]
 replicas = 5
 for d in density:
@@ -33,3 +33,4 @@ for d in density:
         tfb = time.clock()-ti
         print(no_vertices,round((no_vertices-1)*d*no_vertices),r, mf, tf, tfb)
 
+#se almacenaron los resultados en resultadosTarea5/result.pdf donde se ve que el algoritmo tarda mas cuando calcula mas flujo, (y que calculo mas caminos) o cuando aumenta la densidad del grafo. no se pudo contrastar contra el algoritmo de ford fulkerson que escoge un camino al azar ya que demoraba mucho mas tiempo para los experimentos.
