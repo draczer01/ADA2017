56b72785ca48a714cdd687fd63f244fe1e3b66bf
Jose Hernandez <tashiro86@gmail.com>
2017-06-15 22:18:30 -0500

    added some results

--- Tareas/resultadosTarea5/analisisTarea5.R
+++ Tareas/resultadosTarea5/analisisTarea5.R
@@ -1,8 +1,4 @@
 datos = read.csv("resultTarea5.dat", sep=" ", header=FALSE)
-a = aggregate(datos, by=list(datos$V1), mean)
-plot(a$Group.1, a$V2, col="blue", pch=16, type="l",main="Comparación quicksort vs prune and search",xlab="Tamaño cadena (n)", ylab="Cant. de comparaciones")
-lines(a$Group.1, a$V3, col="red", pch=16)
-lines(a$Group.1, (a$Group.1* log(a$Group.1,2)), col="black", pch=16)
-lines(a$Group.1, (a$V1**2), col="green", pch=16)
-legend("topright", inset=.05, title="Comparaciones",
-   c("ps","qs", "nlog(n)","n^2"),lty=c(1,1,1,1),col=c("blue","red","black", "green") )
+a = aggregate(datos, by=list(datos$V2), mean)
+plot(a$Group.1*100/a$V1, a$V5, col="blue", pch=16, type="l",main="Comparación tiempo entre la densidad del grafo",xlab="Densidad", ylab="Tiempo")
+boxplot(datos$V5 ~ datos$V2)
